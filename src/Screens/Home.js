27f528314db25d38886eacc74ccd8ec99a3f0f76
Priyanshu <vishwakarmapriyanshu98@gmail.com>
2024-03-22 00:19:52 +0530

    d-2(21st march)

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
+import Card from '../Components/Card';
+import Carousal from '../Components/Carousal';
 
 export default function Home() {
   return (
     <div>
-    <div><Navbar/></div>
-    <div>Body</div>
-    <div> <Footer/></div>
+      <div><Navbar /></div>
+      <div><Carousal/></div>
+      <div>
+        <Card/>
+      </div>
+      <div> <Footer /></div>
     </div>
   )
 }
